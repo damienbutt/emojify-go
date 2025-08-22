14f19499bc2d09a592831250a3590a31dc1dcc8d
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-08-22 16:50:43 +0100

    ci: simplyfy goreleaser config

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,4 +1,15 @@
 package version
 
-// Version is the current version of emojify-go
-const Version = "1.0.0"
+var (
+	// Version is the current version of emojify-go
+	Version string = "1.0.0"
+
+	// Commit is the git commit hash at build time
+	Commit string
+
+	// Date is the build date
+	Date string
+
+	// BuiltBy is the builder identifier
+	BuiltBy string = "goreleaser"
+)
